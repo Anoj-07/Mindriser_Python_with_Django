d97f94f021da2bfb7a8e7f8216179aa6a7396de6
Anoj <anojrawal12345@gmail.com>
2025-06-12 08:54:49 +0545

    Day_2

---
+++ Day_2/day_2.py
@@ -0,0 +1,114 @@
+#variables - Are continers that store and make data reusable
+
+Hello = "Hello World!"
+
+print(Hello)
+
+
+#----------------------------------------------------------------
+#Identifiers
+
+#Snake Case - snake_case
+#Camel Case - camelCase
+#Pascal Case - PascalCase
+#Kebab Case - kebab-case
+#Upper Case - UPPERCASE
+
+#----------------------------------------------------------------
+# Data types
+
+#String - A sequence of characters - "Hello"
+#Integer - A whole number - 1
+#Float - A decimal number - 1.0
+#Boolean - True or False - True
+
+#List - A collection of items - ordered and changeable - [1, 2, 3]
+#Tuple - An immutable list - ordered and unchangeable - (1, 2, 3)
+#Dictionary - A collection of key-value pairs - unordered and changeable - {"name": "John", "age": 20}
+#Set - A collection of unique items - unordered(randomlly) and unchangeable - {1, 2, 3} - set()
+
+#----------------------------------------------------------------
+#operators
+
+#Arithmetic operators - +, -, *, /, %, **, //  
+#Example:
+#print(1 + 2) -> output: 3 (1+2) - addition
+#print(1 - 2) -> output: -1 (1-2) - subtraction
+#print(1 * 2) -> output: 2 (1*2) - multiplication
+#print(1 / 2) -> output: 0.5 (1/2) - division
+#print(1 % 2) -> output: 1 (1%2) - modulus
+#print(1 ** 2) -> output: 1 (1^2) - exponentiation
+#print(1 // 2) -> output: 0 (1/2) - floor division
+
+#Assignment operators -> =, +=, -=, *=, /=, %=, **=, //=
+#Example:
+#x = 1 -> output: 1  - assignment
+#x += 1 -> output: 2 - addition assignment
+#x -= 1 -> output: 0  - subtraction assignment
+#x *= 1 -> output: 1 - multiplication assignment
+#x /= 1 -> output: 1 - division assignment
+#x %= 1 -> output: 0 - modulus assignment
+#x **= 1 -> output: 1 - exponentiation assignment
+#x //= 1 -> output: 1 - floor division assignment
+
+#Comparison operators - ==, !=, >, <, >=, <=
+#Example:
+#print(1 == 2) -> output: False - equal to
+#print(1 != 2) -> output: True - not equal to
+#print(1 > 2) -> output: False - greater than
+#print(1 < 2) -> output: True - less than
+#print(1 >= 2) -> output: False - greater than or equal to
+#print(1 <= 2) -> output: True - less than or equal to
+
+#Logical operators - and, or, not
+#Example:
+#print(1 and 2) -> output: 2 - and
+#print(1 or 2) -> output: 1 - or
+#print(not 1) -> output: False - not
+
+#Identity operators - is, is not
+#Example:
+#print(1 is 2) -> output: False - is
+#print(1 is not 2) -> output: True - is not
+
+#Membership operators - in, not in
+#Example:
+#print(1 in [1, 2, 3]) -> output: True - in
+#print(1 not in [1, 2, 3]) -> output: False - not in
+
+#Bitwise operators - &, |, ^, ~, <<, >>
+#Example:
+#print(1 & 2) -> output: 0 - bitwise and
+#print(1 | 2) -> output: 3 - bitwise or
+#print(1 ^ 2) -> output: 3 - bitwise xor
+#print(~1) -> output: -2 - bitwise not
+#print(1 << 2) -> output: 4 - bitwise left shift
+#print(1 >> 2) -> output: 0 - bitwise right shift
+
+#----------------------------------------------------------------
+
+#+ can be used to concatenate strings
+#Example:
+#print("Hello" + "World") -> output: HelloWorld
+
+
+#----------------------------------------------------------------
+
+#* can be used to repeat strings
+#Example:
+#print("Hello" * 3) -> output: HelloHelloHello
+#----------------------------------------------------------------
+#input()
+
+#Example:
+#name = input("Enter your name: ")
+#print(f"Hello {name}")
+
+
+
+
+
+
+
+
+
