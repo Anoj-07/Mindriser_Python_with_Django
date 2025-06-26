2504859579b1e741653f7da8a1119c6f91fef405
Anoj <anojrawal12345@gmail.com>
2025-06-26 08:02:20 +0545

    Ecommerce_database

--- day_10_Task_ecommerce.py
+++ day_10_Task_ecommerce.py
@@ -1,7 +1,17 @@
+# Provide register and login to user
+    # During register ask the user if he/she is buyer or seller
+    # In login after login successfull give user choices according to user's type
+        # If buyer : 1. View all products 2. Purchase a product 3. View bills
+        # If seller : 1. View products 2. Add product 3. View bills
+
+
+
 import json
 
-#-------------------------Main--------------------------------------
+
+#-------------------------Main function--------------------------------------------
 def main():
+
     user_input = input('Select the Type of user: 1. Buyer 2. Seller \n')
     if user_input == '1':
         buyer()
@@ -10,7 +20,7 @@ def main():
     else:
         print("Invalid input!")
 
-#-------------------------Login & Register--------------------------
+#-------------------------Login & Register function------------------------------------
 def login(user_type):
     user_choice = input("Enter a choice: 1. Register 2. Login \n")
 
@@ -31,7 +41,7 @@ def login(user_type):
 
                 user_json_data = json.dumps(user_dict_data)
 
-                with open('users.txt', 'a') as f:
+                with open('Database/users.txt', 'a') as f:
                     f.write(user_json_data + '-')
                 break
             else:
@@ -41,7 +51,7 @@ def login(user_type):
             user_username = input("Enter your userName: ")
             user_password = input("Enter your password: ")
 
-            f = open('users.txt', 'r')
+            f = open('Database/users.txt', 'r')
             user_data = f.read()
             f.close()
 
@@ -62,10 +72,10 @@ def login(user_type):
         print('Invalid input!')
         
 
-#-------------------------Buyer--------------------------------------
+#-------------------------Buyer function--------------------------------
 def buyer():
-    success = login('buyer')
-    if success:
+    username = login('buyer')
+    if username:
         while True:
             print("\nBuyer Options:")
             print("1. View all products")
@@ -74,19 +84,146 @@ def buyer():
             print("4. Logout")
             choice = input("Enter your choice: ")
 
+
+            # To view All the produts 
             if choice == '1':
-                print("Viewing all products...")
+                with open('Database/product.txt', 'r') as f:
+                    product_data = f.read()
+                
+                list_product_data = product_data.split('-')
+                list_product_data.remove('')
+
+                if list_product_data:
+                    for i in list_product_data:
+                        product = json.loads(i)
+                        print(f"*****products:*****\n")
+                        print(f"productName: {product['productName']}")
+                        print(f"productName: {product['price']}")
+                        print(f"productName: {product['quantity']}")
+                        print('===============================')
+                else:
+                    print("No products found.")
+
+
             elif choice == '2':
-                print("Purchasing a product...")
+
+                #shows lissted products
+                with open('Database/product.txt', 'r') as f:
+                    product_data = f.read()
+                
+                list_product_data = product_data.split('-')
+                list_product_data.remove('')
+
+                if list_product_data:
+                    for i in list_product_data:
+                        product = json.loads(i)
+                        print(f"*****products:*****\n")
+                        print(f"productName: {product['productName']}")
+                        print(f"price: {product['price']}")
+                        print(f"quantity: {product['quantity']}")
+                        print('===============================')
+                else:
+                    print("No products found.")
+                
+                # buy products logics
+                f = open('Database/product.txt', 'r')
+                buy_data = f.read()
+                f.close()  
+
+                list_bill_data = buy_data.split('-')
+                list_bill_data.remove('')
+
+                products = []
+                for idx, i in enumerate(list_product_data):
+                    product = json.loads(i)
+                    products.append(product)  
+                    print(f"{idx + 1}. productName: {product['productName']} - Price: {product['price']} - Quantity: {product['quantity']}")
+
+
+                        #CODE HERE
+                while True:
+                    select_Input = input("Enter product index to purchase: \n")
+                    if select_Input.isdigit():
+                        index = int(select_Input)
+
+                        if 1 <= index <= len(products):
+                            selected_product = products[index - 1]
+                            print(f"selectd products: {selected_product['productName']}")
+                            break
+                        else:
+                            print('Out of range, try again!')
+                    else:
+                        print("Enter valid number!")
+                
+                while True:
+                    quty = input(f"Enter qnty to buy (Available: {selected_product['quantity']}): ")
+                    if quty.isdigit():
+                        quty_int = int(quty)
+                        if 1 <= quty_int <= selected_product['quantity']:
+                            quty = quty_int
+                            break
+                    print('Give valid quty: ')
+                
+                selected_product['quantity'] -= quty
+
+                f = open('Database/product.txt', 'w')
+                for p in products:
+                    json_str = json.dumps(p)
+                    f.write(json_str + '-')
+                
+                f.close()
+
+                total_price = selected_product['price'] * quty
+
+                bill = {
+                    'productName': selected_product['productName'],
+                    'quantity': quty,
+                    'price': total_price,
+                    'buyer': username,
+                    'seller': selected_product['sellerName'],
+                }
+                
+                bill_dic_json = json.dumps(bill)
+                f = open('Database/bill.txt', 'a')
+                f.write(bill_dic_json + '-')
+
+                print(f"successful! You purchase {quty} of {selected_product['productName']}. ")
+
+                
             elif choice == '3':
-                print("Viewing bills...")
+                print("===== Your Bills=====")
+
+                f = open('Database/bill.txt', 'r')
+                bill_data = f.read()
+                f.close() 
+
+                list_bill_data = bill_data.split('-')
+                list_bill_data.remove('')
+                
+
+                found = False
+                for i in list_bill_data:
+                        bill = json.loads(i)
+                        if bill.get('buyer') == username:
+                                print("\n-------------------")
+                                print(f"Product: {bill['productName']}")
+                                print(f"Quantity: {bill['quantity']}")
+                                print(f"Price: {bill['price']}")
+                                print(f"Seller: {bill['seller']}")
+                                print("-------------------\n")
+                                found = True
+                if not found:
+                            print("No bills found.")
+
+            
             elif choice == '4':
-                print("Logged out.")
+                print("Logged out! \n")
+                print("Visit Again! ")
                 break
             else:
-                print("Invalid choice. Try again.")
+                print("Invalid choice. Give valid input! \n")
 
-#-------------------------Seller--------------------------------------
+#-------------------------Seller-------------------------------------------
 def seller():
     username = login('seller')
     if username:
@@ -99,8 +236,9 @@ def seller():
             print("4. Logout")
             choice = input("Enter your choice: ")
 
+            # Viewing products of sellers
             if choice == '1':
-                f = open('product.txt', 'r')
+                f = open('Database/product.txt', 'r')
                 product_data = f.read()
                 f.close() 
 
@@ -127,8 +265,9 @@ def seller():
                 price = float(input("Enter price (In number) of your product: \n"))
                 quantity = int(input("Enter Quantity (In number) of products: \n"))
 
-                price('Add sucessfully')
 
+                print('Add sucessfull!')
+               
                 seller_dic = {
                     'productName': product_name,
                     'price': price,
@@ -137,19 +276,43 @@ def seller():
                 }
 
                 user_json_data = json.dumps(seller_dic)
-
-                with open('product.txt', 'a') as f:
+                with open('Database/product.txt', 'a') as f:
                     f.write(user_json_data + '-')
                 break
              
 
+             #Bill get from buyer
             elif choice == '3':
-                print("Viewing bills...")
-            elif choice == '4':
-                print("Logged out.")
-                break
-            else:
-                print("Invalid choice. Try again.")
+                print("\n====================== Bills ==============================")
 
-#-------------------------Run Program--------------------------------
+                f = open('Database/bill.txt', 'r')
+                bill_data = f.read()
+                f.close()
+
+                bill_list = bill_data.split('-')
+                bill_list.remove('')
+                
+                found = False
+                for i in bill_list:
+                    bill = json.loads(i)
+                    if bill.get('seller') == username:
+                        print(f"Product : {bill['productName']}")
+                        print(f"Quantity: {bill['quantity']}")
+                        print(f"Price   : {bill['price']}")
+                        print(f"Buyer   : {bill['buyer']}")
+                        found = True
+                
+                if not found:
+                    print("No bill found! ")
+
+                # For logOut
+            elif choice == '4':
+                print("Logged out! \n")
+                print("Visit Again!")
+                break
+            
+            else:
+                print("Invalid choice. choose valid one.")
+
+#-------------------------main start function--------------------------------
 main()
